b596fe95e672ce2c311a7468116b486a8fe409a9
thunder-design <thunderstorm1232@gmail.com>
2020-08-21 17:08:24 -0400

    Update tag.js

--- commands/tag.js
+++ commands/tag.js
@@ -81,7 +81,7 @@ async function showRow(receivedMessage, args, sheet) {
 
     const outputMessage = new Discord.MessageEmbed();
     outputMessage.setColor("#3381ff");
-    outputMessage.setFooter(`Total ${targetRows.length}`);
+    outputMessage.setFooter(`Total: ${targetRows.length}`);
     if (content == "") {
         outputMessage.setTitle(`__**Not found**__`);
         outputMessage.setDescription(`${args[1]} was not found.`)

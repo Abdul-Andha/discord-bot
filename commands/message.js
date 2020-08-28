87f37fa3a475328c05f173b84257d975fdbd841e
thunder-design <thunderstorm1232@gmail.com>
2020-08-28 18:44:16 -0400

    Update message.js

--- commands/message.js
+++ commands/message.js
@@ -1,17 +1,17 @@
 const Discord = require(`discord.js`);
 module.exports = {
     name: 'message',
-    description: 'Requires two arguments. The first argument must be a channel. The second argument must be the message. The bot will send the specified message to the specified channel.',
+    description:'Requires three arguments. The first argument must be a channel. The second argument must be the title. The third argument must be the message. The bot will send the specified message to the specified channel.',
     execute(receivedMessage, args) {
-        if (args.length < 2)
+        if (args.length < 3)
             return receivedMessage.channel.send("Error: Not enough arguments. Try ~message **[Channel]** **[Message]**")
-        if (args.length > 2)
-            for (let i = 2; i < args.length; i++)
-                args[1] += " " + args[i];
+        if (args.length > 3)
+            for (let i = 3; i < args.length; i++)
+                args[2] += " " + args[i];
         const outputMessage = new Discord.MessageEmbed();
-        outputMessage.setColor("");
-        outputMessage.setTitle("");
-        outputMessage.setDescription(args[1]);
+        outputMessage.setColor("#3381ff");
+        outputMessage.setTitle(args[1]);
+        outputMessage.setDescription(args[2]);
         receivedMessage.channel.send(outputMessage);
     }
 }

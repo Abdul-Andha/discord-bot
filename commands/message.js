600e3c8e4050a1809c4c3be01d43a5574af2ea48
thunder-design <thunderstorm1232@gmail.com>
2020-08-28 18:34:28 -0400

    Update message.js

--- commands/message.js
+++ commands/message.js
@@ -1,6 +1,6 @@
 const Discord = require(`discord.js`);
 module.exports = {
-    name: 'Message',
+    name: 'message',
     description: 'Requires two arguments. The first argument must be a channel. The second argument must be the message. The bot will send the specified message to the specified channel.',
     execute(receivedMessage) {
         const outputMessage = new Discord.MessageEmbed();

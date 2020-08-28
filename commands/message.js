9c367f5cc41b979b7db0ef6d394db5de930e12e3
thunder-design <thunderstorm1232@gmail.com>
2020-08-28 18:36:38 -0400

    Update message.js

--- commands/message.js
+++ commands/message.js
@@ -2,11 +2,11 @@ const Discord = require(`discord.js`);
 module.exports = {
     name: 'message',
     description: 'Requires two arguments. The first argument must be a channel. The second argument must be the message. The bot will send the specified message to the specified channel.',
-    execute(receivedMessage) {
+    execute(receivedMessage, args) {
         const outputMessage = new Discord.MessageEmbed();
-        outputMessage.setColor("#3381ff");
-        outputMessage.setTitle("**__TIC-TAC-TOE__**");
-        outputMessage.setDescription("**_**|**_**|**_**\n**_**|**_**|**_**\n**.**|**.**|**.**");
+        outputMessage.setColor("");
+        outputMessage.setTitle("");
+        outputMessage.setDescription(args[2]);
         receivedMessage.channel.send(outputMessage);
     }
 }

3a7699326395f8ebd7b7767e5134bf1b747bde1b
thunder-design <thunderstorm1232@gmail.com>
2020-08-28 18:39:24 -0400

    Update message.js

--- commands/message.js
+++ commands/message.js
@@ -3,10 +3,15 @@ module.exports = {
     name: 'message',
     description: 'Requires two arguments. The first argument must be a channel. The second argument must be the message. The bot will send the specified message to the specified channel.',
     execute(receivedMessage, args) {
+        if (args.length < 2)
+            return receivedMessage.channel.send("Error: Not enough arguments. Try ~message **[Channel]** **[Message]**")
+        if (args.length > 2)
+            for (let i = 2; i < args.length; i++)
+                args[1] += " " + args[i];
         const outputMessage = new Discord.MessageEmbed();
         outputMessage.setColor("");
         outputMessage.setTitle("");
-        outputMessage.setDescription(args[2]);
+        outputMessage.setDescription(args[1]);
         receivedMessage.channel.send(outputMessage);
     }
 }

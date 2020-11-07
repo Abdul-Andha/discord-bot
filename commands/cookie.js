e49066bbff2b80beb6de38127748fcae1d552be5
thunder-design <thunderstorm1232@gmail.com>
2020-11-07 13:35:45 -0500

    Update cookie.js

--- commands/cookie.js
+++ commands/cookie.js
@@ -1,3 +1,5 @@
+<script src="utilities.js"></script>
+
 module.exports = {
     name: 'cookie clicker',
     description: 'Classic Cookie Clicker Game. Consists of creating a profile, doing a command to "click" the cookie, buying upgrades, leaderboard.',

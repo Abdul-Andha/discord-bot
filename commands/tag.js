18671e902d193fe18aea1a46b4624d830f06a1d2
thunder-design <thunderstorm1232@gmail.com>
2020-11-07 13:27:24 -0500

    Update tag.js

--- commands/tag.js
+++ commands/tag.js
@@ -132,14 +132,14 @@ async function updateRow(receivedMessage, args, sheet) {
     return receivedMessage.react('👍');
 }
 
-async function findRow(arg, sheet) {
-    let targetRows = [];
-    let rows = await sheet.getRows();
-    await sheet.loadCells(`A2:F${rows.length + 1}`);
-    for (let i = 1; i < rows.length + 1; i++)
-        for (let j = 0; j < 6; j++)
-            if (arg === sheet.getCell(i, j).value.toString())
-                if (!targetRows.includes(rows[i - 1]))
-                    targetRows.push(rows[i - 1]);
-    return targetRows;
-}
+// async function findRow(arg, sheet) {
+//     let targetRows = [];
+//     let rows = await sheet.getRows();
+//     await sheet.loadCells(`A2:F${rows.length + 1}`);
+//     for (let i = 1; i < rows.length + 1; i++)
+//         for (let j = 0; j < 6; j++)
+//             if (arg === sheet.getCell(i, j).value.toString())
+//                 if (!targetRows.includes(rows[i - 1]))
+//                     targetRows.push(rows[i - 1]);
+//     return targetRows;
+// }

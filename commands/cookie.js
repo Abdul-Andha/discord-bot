ad9bfb4b9308fe649c68232213e8604534592332
thunder-design <thunderstorm1232@gmail.com>
2020-11-07 12:24:49 -0500

    Update cookie.js

--- commands/cookie.js
+++ commands/cookie.js
@@ -2,6 +2,14 @@ module.exports = {
     name: 'cookie clicker',
     description: 'Classic Cookie Clicker Game. Consists of creating a profile, doing a command to "click" the cookie, buying upgrades, leaderboard.',
     execute(receivedMessage) {
-        return receivedMessage.channel.send("test");
+        let temp = {
+            Alias: "Test",
+            Discord: "Testing",
+            Reason: "Testor",
+            Date: "Tested",
+            ID: "Toast",
+            IGN: "Testn't"
+        };
+        sheet.addRow(temp);
     }
 }

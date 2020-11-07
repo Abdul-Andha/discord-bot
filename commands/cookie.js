2cd8e85b2d1cfb24970b96220a804a95e874714c
thunder-design <thunderstorm1232@gmail.com>
2020-11-07 13:00:59 -0500

    Update cookie.js

--- commands/cookie.js
+++ commands/cookie.js
@@ -3,11 +3,11 @@ module.exports = {
     description: 'Classic Cookie Clicker Game. Consists of creating a profile, doing a command to "click" the cookie, buying upgrades, leaderboard.',
     execute(receivedMessage, args, sheet) {
         if (args.length === 0)
-            clickCookie(receivedMessage);
+            clickCookie(receivedMessage, sheet);
     }
 }
 
-async function clickCookie(receivedMessage) {
+async function clickCookie(receivedMessage, sheet) {
     let rows = await sheet.getRows();
     console.log(rows.length);
 }

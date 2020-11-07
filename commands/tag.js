f64d972826a84c54f1c0a0fa7f81a25419cb6e11
thunder-design <thunderstorm1232@gmail.com>
2020-11-07 13:46:28 -0500

    Update tag.js

--- commands/tag.js
+++ commands/tag.js
@@ -62,7 +62,7 @@ async function removeRow(receivedMessage, args, sheet) {
 }
 
 async function showRow(receivedMessage, args, sheet) {
-    console.log("test");
+    console.log("tst");
     if (args.length < 2)
         return receivedMessage.channel.send("Error: Not enough arguments. Try /tag find **Alias**")
     if (args.length > 2)

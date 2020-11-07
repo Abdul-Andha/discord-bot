12dd2ebc249f5d1a535448ce922ddf19cb1b7d9d
thunder-design <thunderstorm1232@gmail.com>
2020-11-07 12:14:18 -0500

    Create cookie.js

---
+++ commands/cookie.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'cookie clicker',
+    description: 'Classic Cookie Clicker Game. Consists of creating a profile, doing a command to "click" the cookie, buying upgrades, leaderboard.',
+    execute(receivedMessage) {
+        return receivedMessage.channel.send("test");
+    }
+}

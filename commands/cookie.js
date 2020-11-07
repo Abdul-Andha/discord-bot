19ec47e59c4c89e9246db831355cfb9ee6cf8325
thunder-design <thunderstorm1232@gmail.com>
2020-11-07 14:17:40 -0500

    Update cookie.js

--- commands/cookie.js
+++ commands/cookie.js
@@ -29,7 +29,7 @@ async function clickCookie(receivedMessage, sheet) {
 async function findRow(arg, sheet) {
     let targetRows = [];
     let rows = await sheet.getRows();
-    await sheet.loadCells(`A2:F${rows.length + 1}`);
+    await sheet.loadCells(`A2:G${rows.length + 1}`);
     for (let i = 1; i < rows.length + 1; i++)
         for (let j = 0; j < 7; j++)
             if (arg === sheet.getCell(i, j).value.toString())

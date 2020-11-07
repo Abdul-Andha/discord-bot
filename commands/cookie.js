523bd85ef5d3c0cf9c2b00c3f4f4b1a9a15ec067
thunder-design <thunderstorm1232@gmail.com>
2020-11-07 13:59:15 -0500

    Update cookie.js

--- commands/cookie.js
+++ commands/cookie.js
@@ -9,9 +9,32 @@ module.exports = {
 
 async function clickCookie(receivedMessage, sheet) {
     let rows = await sheet.getRows();
-    console.log(rows.length);
+    targetRow = findRow(receivedMessage.user.id, sheet);
+    if (targetRow.length === 0) {
+        let newRow = {
+            Name: receivedMessage.user.userName,
+            CookieCount: 1,
+            CookiesPerSec: 0,
+            CookiePerClick: 1,
+            GrandmaCount: 0,
+            Time: "test",
+            ID: receivedMessage.user.id
+        };
+        sheet.addRow(newRow);
+    } 
 }
 
+async function findRow(arg, sheet) {
+    let targetRows = [];
+    let rows = await sheet.getRows();
+    await sheet.loadCells(`A2:F${rows.length + 1}`);
+    for (let i = 1; i < rows.length + 1; i++)
+        for (let j = 0; j < 6; j++)
+            if (arg === sheet.getCell(i, j).value.toString())
+                if (!targetRows.includes(rows[i - 1]))
+                    targetRows.push(rows[i - 1]);
+    return targetRows;
+}
 
 
 // name 

2608c5b914e2eebf95322016392b4d1bbfdf4321
thunder-design <thunderstorm1232@gmail.com>
2021-02-14 16:46:58 -0500

    Update copy.js

--- commands/copy.js
+++ commands/copy.js
@@ -2,8 +2,9 @@ module.exports = {
     name: 'copy',
     description: 'Copies a channel to another channel in another server.',
     execute(bot, receivedMessage, args) {
+        
         let quotesChannel = bot.channels.cache.find(channel => channel.id === "531298362545274930");
-        console.log(quotesChannel.messages.cache);
+        console.log(quotesChannel.messages.fetch({ limit: 10 }));
         let targetChannel = bot.channels.cache.find(channel => channel.id === "547594143807963138");
         targetChannel.send("copy test 2");
     }

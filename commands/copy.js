0092084e8325c1240ea23a03048ce5761bdafb5f
thunder-design <thunderstorm1232@gmail.com>
2021-02-14 16:43:35 -0500

    Update copy.js

--- commands/copy.js
+++ commands/copy.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(bot, receivedMessage, args) {
         let quotesChannel = bot.channels.cache.find(channel => channel.id === "531298362545274930");
         console.log(quotesChannel.messages.cache);
-        // let targetChannel = bot.channels.cache.find(channel => channel.id === "547594143807963138");
-        // targetChannel.send("copy test 2");
+        let targetChannel = bot.channels.cache.find(channel => channel.id === "547594143807963138");
+        targetChannel.send("copy test 2");
     }
 }

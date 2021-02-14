5f4ec9d66881f58e30d640ac1f8d38b977f02075
thunder-design <thunderstorm1232@gmail.com>
2021-02-14 14:08:52 -0500

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -3,15 +3,36 @@ module.exports = {
     name: 'announce',
     description:'Requires three arguments. The first argument must be a channel. The second argument must be the title. The third argument must be the message. The bot will announce the specified message to the specified channel.',
     execute(bot, receivedMessage, args) {
+        let title;
+        let msgStart = 2;
         if (args.length < 3)
-            return receivedMessage.channel.send("Error: Not enough arguments. Try ~announce **[Channel]** **[Title]** **[Message]**");
-        if (args.length > 3)
-            for (let i = 3; i < args.length; i++)
-                args[2] += " " + args[i];
+            return receivedMessage.channel.send("Error: Not enough arguments. Try ~announce **[Channel]** **\"[Title]\"** **[Message]**");
+
+        if (args[1].indexOf("\"") != -1) {
+            title = args[1].substr(1);
+            let end = false;
+            let count = 2;
+            while (!end) {
+                if (count > args.length - 1) {
+                    return receivedMessage.channel.send("Error: No closing quote found. Only use quotes for multiple word titles.");
+                }
+                if (args[count].indexOf("\"") != -1) {
+                    end = true;
+                }
+                title += args[count];
+                count++;
+                msgStart = count;
+            } 
+        } else {
+            title = args[1];
+        }
+        
+        for (let i = msgStart + 1; i < args.length; i++)
+            args[msgStart] += " " + args[i];
         const outputMessage = new Discord.MessageEmbed();
         outputMessage.setColor("#3381ff");
-        outputMessage.setTitle(args[1]);
-        outputMessage.setDescription(args[2]);
+        outputMessage.setTitle(title);
+        outputMessage.setDescription(args[msgStart]);
         args[0] = args[0].substr(2);
         args[0] = args[0].substr(0, args[0].length - 1);
         targetChannel = bot.channels.cache.get(args[0]);

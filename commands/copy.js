e84d4d99d0b0cf42b29212cd680a0ab0c50b0e4e
thunder-design <thunderstorm1232@gmail.com>
2021-02-14 18:02:35 -0500

    Update copy.js

--- commands/copy.js
+++ commands/copy.js
@@ -1,15 +1,22 @@
 const { SystemChannelFlags } = require("discord.js");
+const { testing } = require("googleapis/build/src/apis/testing");
 
 module.exports = {
     name: 'copy',
     description: 'Copies a channel to another channel in another server.',
     execute(bot, receivedMessage, args) {
-        console.log("test1");
-        let quotesChannel = bot.channels.cache.find(channel => channel.id === "531298362545274930");
-        console.log(quotesChannel.messages.fetch());
-        let targetChannel = bot.channels.cache.find(channel => channel.id === "547594143807963138");
-        targetChannel.send("copy test 2");
-        targetChannel.send(quotesChannel.messages.fetch().content);
-        console.log("test");
+        testing(bot);
+        
     }
+}
+
+async function testing(bot) {
+    console.log("test1");
+    let quotesChannel = bot.channels.cache.find(channel => channel.id === "531298362545274930");
+    let test = await quotesChannel.messages.fetch();
+    console.log(test);
+    let targetChannel = bot.channels.cache.find(channel => channel.id === "547594143807963138");
+    targetChannel.send("copy test 2");
+    targetChannel.send(test.content);
+    console.log("test");
 }

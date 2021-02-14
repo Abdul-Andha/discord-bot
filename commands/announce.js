36aa564111163a28003dda351465c58ba41ce0bc
thunder-design <thunderstorm1232@gmail.com>
2021-02-14 14:14:33 -0500

    Update announce.js

--- commands/announce.js
+++ commands/announce.js
@@ -17,9 +17,11 @@ module.exports = {
                     return receivedMessage.channel.send("Error: No closing quote found. Only use quotes for multiple word titles.");
                 }
                 if (args[count].indexOf("'") != -1) {
+                    title += args[count].substr(0, args[count].length - 1);
                     end = true;
+                } else {
+                    title += args[count];
                 }
-                title += args[count];
                 count++;
                 msgStart = count;
             } 

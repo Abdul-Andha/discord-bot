dd276ab50ca5dcbf46fbaac0d2eeed4f0484c63b
thunder-design <thunderstorm1232@gmail.com>
2021-02-14 18:14:09 -0500

    Update copy.js

--- commands/copy.js
+++ commands/copy.js
@@ -12,7 +12,7 @@ module.exports = {
 async function testing(bot) {
     console.log("test1");
     let quotesChannel = bot.channels.cache.find(channel => channel.id === "531298362545274930");
-    let test = await quotesChannel.messages.fetch();
+    let test = await quotesChannel.messages.fetch({ limit: 10 });
     console.log(test);
     let targetChannel = bot.channels.cache.find(channel => channel.id === "547594143807963138");
     targetChannel.send("copy test 2");
